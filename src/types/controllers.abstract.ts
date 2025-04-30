3f41180361b8f67edbad3840ab7cc2bba03d0c7b
SavaFeeD <shachnev.02@mail.com>
2025-05-01 03:09:49 +0700

    Update game.controller params by method addEntity

--- src/types/controllers.abstract.ts
+++ src/types/controllers.abstract.ts
@@ -1,7 +1,6 @@
 import { IEntity } from "./entity.interface";
-import { E_RENDERING_CONTEXT_ID } from "./rendering-context.enum";
 
 export abstract class AbstractGameController {
   abstract test(): void;
-  abstract addEntity(entity: IEntity<E_RENDERING_CONTEXT_ID>)
+  abstract addEntity(entity: IEntity<any>): void;
 }
