650fd2b9dca282f5cbfb2cfa5b06bc1a66995743
SavaFeeD <shachnev.02@mail.com>
2025-05-10 04:02:15 +0700

    Added pause and resume to abstract class

--- src/types/controllers.abstract.ts
+++ src/types/controllers.abstract.ts
@@ -8,4 +8,6 @@ export abstract class AbstractGameController {
   abstract test(): void;
   abstract addEntity(entity: IEntity<any>): void;
   abstract run(context: E_RENDERING_CONTEXT_ID): void;
+  abstract pause(): void;
+  abstract resume(context: E_RENDERING_CONTEXT_ID): void;
 }
